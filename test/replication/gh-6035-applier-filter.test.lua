5371a679d94d7f96136b65fc8d858c1b2d571e72
Cyrill Gorcunov <gorcunov@gmail.com>
2021-05-17 14:24:15 +0300

    start test
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

---
+++ test/replication/gh-6035-applier-filter.test.lua
@@ -0,0 +1,155 @@
+test_run = require('test_run').new()
+
+cfg_save = {                                                            \
+    replication_connect_quorum  = box.cfg.replication_connect_quorum,   \
+    replication_synchro_quorum  = box.cfg.replication_synchro_quorum,   \
+    replication_synchro_timeout = box.cfg.replication_synchro_timeout,  \
+    replication_sync_timeout    = box.cfg.replication_sync_timeout,     \
+    read_only                   = box.cfg.read_only,                    \
+    election_mode               = box.cfg.election_mode,                \
+}
+
+----
+---- Instance 1.
+----
+---- Step 1.
+--box.cfg{
+--    listen = 3313,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315', 'localhost:3316'},
+--    replication_connect_quorum = 3,
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    read_only = false,
+--    election_mode = 'candidate',
+--}
+--
+---- Step 5.
+--box.schema.user.grant('guest', 'super')
+--box.schema.create_space('async')
+--_ = box.space.async:create_index('pk')
+--box.schema.create_space('sync', {is_sync = true})
+--_ = box.space.sync:create_index('pk')
+--
+---- Step 11.
+--box.cfg{replication_synchro_quorum = 1}
+--box.ctl.wait_rw()
+--
+---- Step 12.
+--box.cfg{replication_synchro_quorum = 2}
+--require('fiber').create(function() box.space.sync:replace{1} end)
+---- Wait WAL write.
+--os.exit(0)
+--
+---- Step 16.
+---- Restart.
+--box.cfg{
+--    listen = 3313,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315'},
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    election_mode = 'manual',
+--}
+--
+---- Step 18.
+--box.cfg{replication_synchro_quorum = 1}
+--box.cfg{election_mode = 'candidate'}
+--
+--
+--
+--
+----
+---- Instance 2.
+----
+---- Step 2.
+--box.cfg{
+--    listen = 3314,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315', 'localhost:3316'},
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    read_only = true,
+--    election_mode = 'voter',
+--}
+--
+---- Step 7.
+--os.exit(0)
+--
+---- Step 13.
+--box.cfg{
+--    listen = 3314,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315'},
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    election_mode = 'voter',
+--}
+--
+---- Step 15.
+--box.cfg{election_mode = 'manual'}
+--box.ctl.promote()
+--
+---- Step 17.
+---- Resign.
+--box.cfg{election_mode = 'voter'}
+--
+--
+--
+--
+--
+----
+---- Instance 3.
+----
+---- Step 3.
+--box.cfg{
+--    listen = 3315,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315', 'localhost:3316'},
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    read_only = true,
+--    election_mode = 'voter',
+--}
+--
+---- Step 8.
+--os.exit(0)
+--
+---- Step 14.
+--box.cfg{
+--    listen = 3315,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315'},
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    election_mode = 'voter',
+--}
+--
+--
+--
+--
+----
+---- Instance 4.
+----
+---- Step 4.
+--box.cfg{
+--    listen = 3316,
+--    replication = {'localhost:3313', 'localhost:3314', 'localhost:3315', 'localhost:3316'},
+--    replication_connect_quorum = 3,
+--    replication_synchro_quorum = 2,
+--    replication_synchro_timeout = 1000,
+--    replication_sync_timeout = 5,
+--    read_only = true,
+--    election_mode = 'voter',
+--}
+--
+---- Step 6.
+--box.cfg{read_only = false, election_mode = 'manual'}
+--box.ctl.promote()
+--
+---- Step 9.
+--box.space.async:replace{1}
+---- Wait replication to instance 1.
+--
+---- Step 10.
+--os.exit(0)
