b628f91c3e72cffb93c159f1ffb25e337c369100
Georgiy Lebedev <curiousgeorgiy@gmail.com>
2021-08-18 21:38:02 +0300

    box, sql: introduce llvm_jit_bootstrap.c module generation file
    Introduce source file for generating LLVM JIT bootstrap module. Embed generated bitcode with bin_source function.

---
+++ src/box/sql/llvm_jit_bootstrap.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019-2021, Tarantool AUTHORS, please see AUTHORS file.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * This source file never gets linked. It is only used to generate LLVM bitcode
+ * for the LLVM JIT bootstrap module.
+ */
+
+#include "sqlInt.h"
+#include "mem.h"
+#include "vdbeInt.h"
+
+struct Vdbe vdbe_type;
+struct vdbe_field_ref vdbe_field_ref_type;
+struct Mem mem_type;
+struct func func_type;
+
+void *referenced_fns[] = {
+	mem_copy,
+	mem_set_null,
+	mem_set_int,
+	mem_set_bool,
+	mem_set_double,
+	mem_set_str0_static,
+	vdbe_op_next,
+	vdbe_op_realify,
+	vdbe_op_column,
+	vdbe_op_fetch,
+	vdbe_op_aggstep0,
+	vdbe_op_aggstep,
+	vdbe_op_aggfinal,
+};
