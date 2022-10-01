afc53dfbe2afec6ef70ca7c6b85e6528ef6d041b
Georgiy Lebedev <g.lebedev@tarantool.org>
2022-10-01 16:08:08 +0300

    memtx: fix use-after-free of successor in `tree_iterator_start`
    We assumed that the successor tuple's story could not get garbage collected
    on clarify of result tuple in `tree_iterator_start`, since they coincide in
    case of regular iterators. But this is not the case for reverse iterators:
    the result tuple is of-by-one from the successor, which means the
    successor's story can get garbage collected along with the tuple itself
    getting deleted, leading to use-after-free of successor: remove garbage
    collection from `memtx_tx_tuple_clarify` and call it manually.
    
    The crash in #7756 revealed that the `put` in transaction manager's story
    hash table was performed incorrectly: fix it and add an assertion that
    nothing was replaced.
    
    Closes #7755
    Closes #7756
    
    NO_DOC=bugfix

--- src/box/memtx_hash.cc
+++ src/box/memtx_hash.cc
@@ -171,8 +171,9 @@ hash_iterator_eq(struct iterator *it, struct tuple **ret)
 		return 0;
 	struct txn *txn = in_txn();
 	struct space *sp = space_by_id(it->space_id);
-/********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND START*********/
 	*ret = memtx_tx_tuple_clarify(txn, sp, *ret, it->index, 0);
+/********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND START*********/
+	memtx_tx_story_gc();
 /*********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND END**********/
 	return 0;
 }
@@ -303,6 +304,9 @@ memtx_hash_index_random(struct index *base, uint32_t rnd, struct tuple **result)
 		*result = light_index_get(hash_table, k);
 		assert(*result != NULL);
 		*result = memtx_tx_tuple_clarify(txn, space, *result, base, 0);
+/********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND START*********/
+		memtx_tx_story_gc();
+/*********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND END**********/
 	} while (*result == NULL);
 	return memtx_prepare_result_tuple(result);
 }
@@ -333,8 +337,9 @@ memtx_hash_index_get_internal(struct index *base, const char *key,
 	uint32_t k = light_index_find_key(&index->hash_table, h, key);
 	if (k != light_index_end) {
 		struct tuple *tuple = light_index_get(&index->hash_table, k);
-/********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND START*********/
 		*result = memtx_tx_tuple_clarify(txn, space, tuple, base, 0);
+/********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND START*********/
+		memtx_tx_story_gc();
 /*********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND END**********/
 	} else {
 /********MVCC TRANSACTION MANAGER STORY GARBAGE COLLECTION BOUND START*********/
