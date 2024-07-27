0213b6d71e7ee6b3b916d96c3cda2156f3f1d84a
Astronomax <fgfgfb93@gmail.com>
2024-07-27 13:27:22 +0300

    replication: infinite `replication_synchro_timeout`
    This commit allows the `replication_synchro_timeout` option to be set to
    0, which means an infinite timeout. When set to 0, the transaction will
    never be rolled back due to timeout, only PROMOTE and DEMOTE can rollback
    it in this case. This behavior is more consistent with the standard Raft
    design.
    
    Part of #7486
    
    @TarantoolBot document
    Title: new possible value for 'replication_synchro_timeout': 0 (infinity)
    Product: Tarantool
    Since: 3.2
    
    Root document: https://www.tarantool.io/ru/doc/latest/reference/configuration/#cfg-replication-replication-synchro-timeout
    
    Now it is allowed to set the option value to 0, which means an infinite
    timeout (the transaction will not be rolled back due to a timeout).
    
    NO_CHANGELOG=Will be added in another commit

--- src/box/gc.c
+++ src/box/gc.c
@@ -57,6 +57,7 @@
 #include "wal.h"		/* wal_collect_garbage() */
 #include "checkpoint_schedule.h"
 #include "txn_limbo.h"
+#include "replication.h"
 
 struct gc_state gc;
 
@@ -522,6 +523,11 @@ gc_do_checkpoint(bool is_scheduled)
 	 * Wait the confirms on all "sync" transactions before
 	 * create a snapshot.
 	 */
+	if (!txn_limbo_is_empty(&txn_limbo)) {
+		say_info("Checkpoint will be created as soon as "
+			 "all existing synchronous transactions "
+			 "are committed");
+	}
 	rc = txn_limbo_wait_confirm(&txn_limbo);
 	if (rc != 0)
 		goto out;
